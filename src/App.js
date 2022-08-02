0edda5cfae8bd54ce5f21bdaeed5e4abe55192ae
Farida-Hamid <faridaaddala@gmail.com>
2022-08-02 15:29:15 +0300

    Add skeleton function for missions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from "./components/header";
 import Rockets from './components/rockets';
+import Missions from './components/missions';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
         </Routes>
       </main>
     </div>
---
+++ src/components/missions.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Missions () {
+  return (
+    <h1>Missions page</h1>
+  );
+}
+
+export default Missions;
