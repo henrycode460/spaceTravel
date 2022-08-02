756d489bcf605ddee700791c64a74dcc03d65bb5
Farida-Hamid <faridaaddala@gmail.com>
2022-08-02 15:27:58 +0300

    Add skeleton function for rockets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from "./components/header";
+import Rockets from './components/rockets';
 
 function App() {
   return (
     <div>
       <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+        </Routes>
+      </main>
     </div>
   );
 }
