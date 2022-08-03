ef08a841ee9e1e9b43fd4b6acc17bf88de958105
Farida-Hamid <faridaaddala@gmail.com>
2022-08-03 13:47:49 +0300

    Underline chosen rout in navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,30 @@
 import React from 'react';
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import mainLogo from "../image/home.png";
 
 function Header() {
-  let activeStyle = {
-    textDecoration: "underline",
-  };
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split('/');
 
   return (
     <nav>
       <img  src={mainLogo} style={ { width: '50px' } } alt="home"/>
       <NavLink
         to="/"
-        style={({ isActive }) =>
-          isActive ? activeStyle : undefined
-        }
+        className={splitLocation[1] === '' ? 'active' : 'inactive'}
         >
         Rockets|
       </NavLink>
       <NavLink
         to="missions"
-        style={({ isActive }) =>
-          isActive ? activeStyle : undefined
-        }
+        className={splitLocation[1] === 'missions' ? 'active' : 'inactive'}
         >
         Missions|
       </NavLink>
       <NavLink
         to="myprofile"
-        style={({ isActive }) =>
-          isActive ? activeStyle : undefined
-        }
+        className={splitLocation[1] === 'myprofile' ? 'active' : 'inactive'}
         >
         My Profile
       </NavLink>
