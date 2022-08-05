5922e40292a226c34434dfeb226735e2e88b8eec
Henry <dokinruss@gmail.com>
2022-08-05 17:26:49 +0000

    update Rockert reducer

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,4 +1,6 @@
 const RETRIEVE = 'spaceTravel/Rockert/RETRIEVE';
+const RESERVE = 'spaceTravel/Rockert/RESERVE';
+const REMOVE = 'spaceTravel/Rockert/REMOVE';
 
 const baseURL = 'https://api.spacexdata.com/v3/rockets';
 
@@ -8,11 +10,34 @@ export const read = (rockerts) => ({
   payload: rockerts,
 });
 
+export const reserve = (id) => ({
+  type: RESERVE,
+  payload: id,
+});
+
+export const remove = (id) => ({
+  type: REMOVE,
+  payload: id,
+});
+
 // Reducer
 const rockertsReducer = (state = [], action) => {
+  let newState;
   switch (action.type) {
     case RETRIEVE:
       return action.payload;
+    case RESERVE:
+      newState = state.map((item) => {
+        if (item.rockerts_id !== action.payload) return item;
+        return { ...item, reserved: true };
+      });
+      return newState;
+    case REMOVE:
+      newState = state.map((item) => {
+        if (item.rockerts_id !== action.payload) return item;
+        return { ...item, reserved: false };
+      });
+      return newState;
     default:
       return state;
   }
@@ -29,11 +54,10 @@ export const recieveRockerts = () => async (dispatch) => {
           rockerts_name: rockerts[key].rocket_name,
           description: rockerts[key].description,
           image: rockerts[key].flickr_images[0],
-
+          reserved: false,
         });
       });
       dispatch(read(rockertsList));
-      console.log('fetched rockets',rockertsList);
     }));
 
 };
