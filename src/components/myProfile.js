7ebe0cef7d62d975f8c6805d72130f7f31a72fa8
Farida-Hamid <faridaaddala@gmail.com>
2022-08-05 12:36:02 +0300

    Style my profile page

--- src/components/myProfile.js
+++ src/components/myProfile.js
@@ -1,24 +1,25 @@
 /* eslint-disable react/jsx-key */
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './myProfile.css';
 
 function MyProfile () {
   const joinedMissions = useSelector(state => state.filter((mission) => mission.reserved));
   const rockets = ['rocket1', 'rocket2', 'rocket3'];
 
   return (
-    <div>
-      <div>
-        <h2>My Missions</h2>
+    <div className='myProfile'>
+      <div className='container'>
+          <h2>My Missions</h2>
+            <ul>
+              {joinedMissions.map((mission) => <li>{mission.mission_name}</li>)}
+            </ul>        
+        </div>
+        <div className='container'>
+          <h2>My Rockets</h2>
           <ul>
-            {joinedMissions.map((mission) => <li>{mission.mission_name}</li>)}
-          </ul>        
-      </div>
-      <div>
-        <h2>My Rockets</h2>
-        <ul>
-        {rockets.map((rocket) => <li>{rocket}</li>)}
-        </ul>
+          {rockets.map((rocket) => <li>{rocket}</li>)}
+          </ul>
       </div>
     </div>
   );
